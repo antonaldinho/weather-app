e36c5493744bf8210cdc26fcb6c98e0787fbd0f7
Antonio Aleman <antonio.9714@gmail.com>
2019-04-07 23:53:45 -0500

    lol

--- credentials.js
+++
@@ -1,2 +0,0 @@
-module.exports.coordKey = 'pk.eyJ1IjoiYW50b25hbGRpbmhvIiwiYSI6ImNqc3pkcGpiZDAwYzk0NGttam85d3ljOG4ifQ.iP3m5bKRfpSKLv4KS0NoxA';
-module.exports.darkskyKey = 'ab0444822ac43847e3171709cae2b72b';
